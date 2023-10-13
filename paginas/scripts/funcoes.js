2646d5c9b1f3cf822658951f1cd7e97742280345
Heitor Pimentel <130229709+heitorpimentel@users.noreply.github.com>
2023-10-13 17:29:08 -0300

    Adicionando botão mostrar senha ao cadastro

--- paginas/scripts/funcoes.js
+++ paginas/scripts/funcoes.js
@@ -14,3 +14,40 @@ function esconderData(){
         }
     });
   }
+
+  /* Cadastro */
+  function mostrarSenha(){
+    var senha = document.querySelector('#inputSenha')
+    if(senha.type === "password"){
+        senha.type = "text"
+    } else if (senha.type === "text"){
+        senha.type = "password";
+    }
+}
+
+    const formataCPF = event => {
+    let input = event.target;
+    input.value = cpf(input.value);
+    };
+
+    const cpf = value => {
+    if (!value) return "";
+    value = value.replace(/\D/g, '');
+    value = value.replace(/(\d{3})(\d)/, "$1.$2");
+    value = value.replace(/(\d{3})(\d)/, "$1.$2");
+    value = value.replace(/(\d)(\d{2})$/, "$1-$2");
+    return value;
+    };
+    
+    const formataTEL = event => {
+    let input = event.target;
+    input.value = telefone(input.value);
+    };
+
+    const telefone = value => {
+    if (!value) return "";
+    value = value.replace(/\D/g, '');
+    value = value.replace(/(\d{2})(\d)/, "($1) $2");
+    value = value.replace(/(\d)(\d{4})$/, "$1-$2");
+    return value;
+    };
