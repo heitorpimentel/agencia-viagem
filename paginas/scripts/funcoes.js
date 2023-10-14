be6693148d8cd1a02009577d103292f2d8bec509
Heitor Pimentel <130229709+heitorpimentel@users.noreply.github.com>
2023-10-13 23:05:17 -0300

    Corrigindo erro na tela cadastro

--- paginas/scripts/funcoes.js
+++ paginas/scripts/funcoes.js
@@ -35,7 +35,7 @@ function esconderData(){
     value = value.replace(/\D/g, '');
     value = value.replace(/(\d{3})(\d)/, "$1.$2");
     value = value.replace(/(\d{3})(\d)/, "$1.$2");
-    value = value.replace(/(\d)(\d{2})$/, "$1-$2");
+    value = value.replace(/(\d{3})(\d{2})$/, "$1-$2");
     return value;
     };
     
